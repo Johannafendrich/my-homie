47b4e97a721514d7f8b6b1848a9e1d320a7c6e3e
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-11 19:50:36 +0200

    Add Buttons component

---
+++ client/src/stories/Button.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from '../components/Button';
+import { action } from '@storybook/addon-actions';
+
+export default {
+  title: 'Buttons',
+};
+export const loginButton = () => (
+  <Button onClick={action('link to Login')}>Login</Button>
+);
+
+export const createButton = () => (
+  <Button onClick={action('link to Account')}>Create account</Button>
+);
+
+export const profileButton = () => (
+  <Button onClick={action('link to Profile')}>To my profile</Button>
+);
+
+export const submitButton = () => (
+  <Button onClick={action('clicked')}>Submit</Button>
+);
+
+export const addHomieButton = () => (
+  <Button onClick={action('clicked')}>Add as Homie</Button>
+);
