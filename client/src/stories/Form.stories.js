ac29955c92d77a7ead0b600a3e82f0aec2cbf04b
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-13 18:01:44 +0200

    Add Sign Up area

--- client/src/stories/Form.stories.js
+++ client/src/stories/Form.stories.js
@@ -4,6 +4,8 @@ import Input from '../components/Input';
 import InputContainer from '../components/InputContainer';
 import EmailAtIcon from '../assets/email-at.svg';
 import PasswordIcon from '../assets/password.svg';
+import UserIcon from '../assets/user.svg';
+import LocationIcon from '../assets/pin.svg';
 
 export default {
   title: 'Login',
@@ -11,6 +13,27 @@ export default {
 
 export const loginForm = () => (
   <Form>
+    <InputContainer>
+      <img src={EmailAtIcon} alt="email" />
+      <Input type="email" placeholder="E-mail" />
+    </InputContainer>
+    <InputContainer>
+      <img src={PasswordIcon} alt="password" />
+      <Input type="password" placeholder="Password" />
+    </InputContainer>
+  </Form>
+);
+
+export const signUpForm = () => (
+  <Form>
+    <InputContainer>
+      <img src={UserIcon} alt="name" />
+      <Input type="text" placeholder="Name" />
+    </InputContainer>
+    <InputContainer>
+      <img src={LocationIcon} alt="name" />
+      <Input type="text" placeholder="City, country" />
+    </InputContainer>
     <InputContainer>
       <img src={EmailAtIcon} alt="email" />
       <Input type="email" placeholder="E-mail" />
