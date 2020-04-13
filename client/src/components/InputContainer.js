dac530f9c2047969cdf66482e4a3e4124fc6ff0e
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-13 13:11:57 +0200

    Add inputContainer

---
+++ client/src/components/InputContainer.js
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+const InputContainer = styled.div`
+  display:flex;
+  flex-direction: row;
+  justify-content flex-start;
+  align-item: baseline;
+  width: 269px;
+  height: 47px;
+  border: 1.5px solid #707070;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin: 10px 20px;
+  outline: none;
+`;
+export default InputContainer;
