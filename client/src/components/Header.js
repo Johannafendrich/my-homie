cd3360181131e556fa783946bc43ea14d37cad0b
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-20 19:55:09 +0200

    Fixed position

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,15 +7,16 @@ const HeaderContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
   height: 65px;
-  margin: 0;
-  padding: 0;
+  margin: 0px;
+  width: 100%;
+  position: fixed;
+  top: 0;
   background-color: white;
   box-shadow: 0px 6px 10px rgba(0, 0, 0, 10%);
 `;
 
-export const Header = () => (
+const Header = () => (
   <HeaderContainer>
     <SmallLogo />
     <Title>MyHomie</Title>
