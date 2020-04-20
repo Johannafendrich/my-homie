bd6f3fcb5d2e54865dbb7f7a0cfbe6e3a6409e4a
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-20 09:03:50 +0200

    optimize Button UI/UX behaviour

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -8,15 +8,15 @@ const Buttons = styled.button`
   padding: 0.7rem;
   border: none;
   border-radius: 20px;
-  box-shadow: 0px 10px 15px rgba(0, 0, 0, 16%);
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 16%);
   text-transform: uppercase;
   font: bold 0.8rem 'Oswald', sans-serif;
-
+  outline: none;
   cursor: pointer;
 
   &:active {
-    transform: translateY(5px) scale(1.1);
-    box-shadow: 0px 10px 15px rgba(134, 69, 255, 60%);
+    transform: translateY(1px) scale(0.9);
+    box-shadow: 0px 5px 10px rgba(134, 69, 255, 60%);
   }
 `;
 
