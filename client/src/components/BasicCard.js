7b97bf91c22a21b11d92edddffcf69795c0af2e5
Johannafendrich <60884265+Johannafendrich@users.noreply.github.com>
2020-04-20 22:37:03 +0200

    Add basic card component (#34)
    * Add basic card component
    
    * fix naming

---
+++ client/src/components/BasicCard.js
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const BasicCard = styled.card`
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-content: center;
+  align-items: left;
+  margin: 0.3rem;
+  padding: 1rem;
+  background-color: white;
+  border-radius: 20px;
+  color: #323338;
+  box-shadow: 0px 10px 15px rgba(0, 0, 0, 16%);
+`;
+export default BasicCard;
