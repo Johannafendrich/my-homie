46fc0c3191d74b21dd1887a5524b9f0edf8f4fd3
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-20 13:35:02 +0200

    Update button stories

--- client/src/stories/Button.stories.js
+++ client/src/stories/Button.stories.js
@@ -1,26 +1,37 @@
 import React from 'react';
 import Button from '../components/Button';
+import Container from '../components/Container';
 import { action } from '@storybook/addon-actions';
 
 export default {
   title: 'Buttons',
 };
 export const loginButton = () => (
-  <Button onClick={action('link to Login')} label="Login" />
+  <Container>
+    <Button onClick={action('link to Login')}>Login</Button>
+  </Container>
 );
 
 export const createAccountButton = () => (
-  <Button onClick={action('link to Account')} label="Create account" />
+  <Container>
+    <Button onClick={action('link to Account')}>Create account</Button>
+  </Container>
 );
 
 export const profileButton = () => (
-  <Button onClick={action('link to Profile')} label="To my profile" />
+  <Container>
+    <Button onClick={action('link to Profile')}>To my profile</Button>
+  </Container>
 );
 
 export const submitButton = () => (
-  <Button onClick={action('clicked')} label="Submit" />
+  <Container>
+    <Button onClick={action('link to Profile')}>Submit</Button>
+  </Container>
 );
 
 export const addHomieButton = () => (
-  <Button onClick={action('clicked')} label="Add as Homie" />
+  <Container>
+    <Button onClick={action('clicked')}>Add as Homie</Button>
+  </Container>
 );
