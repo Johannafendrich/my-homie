d3bcdae3036664e51fa8b2d379ad5dd6dd1b22a3
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-20 19:16:34 +0200

    Add basic card component

---
+++ client/src/stories/BasicCard.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import BasicCard from '../components/BasicCard';
+
+export default {
+  title: 'BasicCard',
+};
+
+export const basicCard = () => (
+  <BasicCard>The content of the page will displayed here</BasicCard>
+);
