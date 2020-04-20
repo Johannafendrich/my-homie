921d1ee56d519853c957031badd19d0b55a3d8b0
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-20 13:34:26 +0200

    Refactoring button component to create generic button

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,8 +1,6 @@
-import React from 'react';
 import styled from '@emotion/styled';
-import PropTypes from 'prop-types';
 
-const Buttons = styled.button`
+const Button = styled.button`
   background-color: #8645ff;
   color: white;
   padding: 0.7rem;
@@ -19,24 +17,5 @@ const Buttons = styled.button`
     box-shadow: 0px 5px 10px rgba(134, 69, 255, 60%);
   }
 `;
-
-const ButtonSize = styled.div`
-  display: flex;
-  justify-content: center;
-  > * {
-    flex-basis: 50%;
-  }
-`;
-
-const Button = ({ label, onClick }) => (
-  <ButtonSize>
-    <Buttons onClick={(event) => onClick(event)}>{label}</Buttons>
-  </ButtonSize>
-);
-
-Button.propTypes = {
-  label: PropTypes.string.isRequired,
-  onClick: PropTypes.func,
-};
 
 export default Button;
