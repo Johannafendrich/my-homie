c9711f8482260ea3a8dbabc33c1c25a0e469f493
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-21 16:44:29 +0200

    Create story for preview

---
+++ client/src/stories/MultiSelect.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MultiSelectInput from '../components/MultiSelect';
+
+export default {
+  title: 'MultiSelectInput',
+};
+
+export const multiSelect = () => (
+  <>
+    <MultiSelectInput />
+  </>
+);
