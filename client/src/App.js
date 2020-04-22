92c667c852675a2b2cfb8acd5a7beb473cee38d5
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-22 08:41:33 +0200

    Add pages path to router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import GlobalStyles from './components/GlobalStyles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <Router>
       <GlobalStyles />
       <Switch>
-        <Route />
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/SignUp">
+          <SignUp />
+        </Route>
+        <Route path="/Home">
+          <Home />
+        </Route>
+        <Route path="/Profile">
+          <Profile />
+        </Route>
       </Switch>
     </Router>
   );
