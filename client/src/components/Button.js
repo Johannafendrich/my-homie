73314a631d7802b4bff135b622d148dd04489f55
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-23 08:37:14 +0200

    Optimize Button styles

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -4,18 +4,20 @@ const Button = styled.button`
   background-color: #8645ff;
   color: white;
   padding: 12px;
+  margin-top: 40px;
+  margin-bottom: 10px;
   border: none;
-  border-radius: 20px;
-  box-shadow: 0px 10px 15px rgba(0, 0, 0, 16%);
+  border-radius: 1.5rem;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 16%);
   text-transform: uppercase;
-  font-size: 14px;
-  font-family: Oswald, sans-serif;
-  font-weight: bold;
+  font: bold 0.8rem 'Oswald', sans-serif;
+  outline: none;
   cursor: pointer;
 
   &:active {
-    transform: translateY(5px) scale(1.1);
-    box-shadow: 0px 10px 15px rgba(134, 69, 255, 60%);
+    transform: translateY(2px) scale(0.9);
+    box-shadow: 0px 5px 10px rgba(135, 70, 255, 60%);
   }
 `;
+
 export default Button;
