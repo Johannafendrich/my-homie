8bf071169ffbb27302193beb315a6a6b2f37a691
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-23 08:22:42 +0200

    Add Background colors

--- client/src/components/GlobalStyles.js
+++ client/src/components/GlobalStyles.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
+import Background from '../assets/background.svg';
 
 function GlobalStyles() {
   return (
@@ -11,14 +12,18 @@ function GlobalStyles() {
           box-sizing: border-box;
           @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@700&family=Roboto:wght@400;500&display=swap');
         }
-        body {
-          margin: 0;
-          height: 100vh;
-          font-family: 'Roboto', sans-serif, regular;
-          font-size: 16px;
-          color: #323338;
-          display: flex;
-          justify-content: center;
+          body {
+            margin: 0px;
+            height: 100vh;
+            font-family: 'Roboto', sans-serif, regular;
+            font-size: 16px;
+            color: #323338;
+            display: flex;
+            justify-content: center;
+            background-image: url(${Background});
+            background-repeat: no-repeat;
+            background-size: cover;        
+
           }
         }
       `}
