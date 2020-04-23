b7c757d0872d6646607af2e28b08c3990d69489b
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-23 08:15:54 +0200

    Add main componentto App

---
+++ client/src/components/Main.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const Main = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  flex-flow: column nowrap;
+  align-items: center;
+  margin: 0;
+`;
+
+export default Main;
