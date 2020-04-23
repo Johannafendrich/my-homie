07cf4e8d3f62942a2c5383b87f4f2247ad364981
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-23 08:33:31 +0200

    optimize Basic Card styles

--- client/src/components/BasicCard.js
+++ client/src/components/BasicCard.js
@@ -1,15 +1,16 @@
 import styled from '@emotion/styled';
 
-export const BasicCard = styled.card`
+const BasicCard = styled.div`
   display: flex;
   flex-flow: column wrap;
   justify-content: center;
   align-content: center;
   align-items: left;
-  margin: 0.3rem;
+  min-width: 95%;
+  margin: 4rem 0.3rem;
   padding: 1rem;
   background-color: white;
-  border-radius: 20px;
+  border-radius: 1.5rem;
   color: #323338;
   box-shadow: 0px 10px 15px rgba(0, 0, 0, 16%);
 `;
