ccc2903cc4ca2da1084097f42d5c545d2fa21608
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-27 11:43:30 +0200

    Undo server test

--- server.js
+++ server.js
@@ -5,24 +5,23 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-// const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
-// server.use(
-//   jsonServer.rewriter({
-//     '/api/*': '/$1',
-//   })
-// );
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+);
 server.use(middlewares);
 server.use(router);
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(path.join(__dirname, 'client/build')));
 }
-//   server.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//   });
-// }
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
-// server.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// })
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
