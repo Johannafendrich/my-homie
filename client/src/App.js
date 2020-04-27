90e65c176d509547894c1cda39757fd6dfbf5c82
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-27 23:08:12 +0200

    Add start page to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GlobalStyles from './components/GlobalStyles';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Start from './pages/Start';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
@@ -14,6 +15,9 @@ function App() {
       <Main>
         <Switch>
           <Route exact path="/">
+            <Start />
+          </Route>
+          <Route exact path="/login">
             <Login />
           </Route>
           <Route path="/signUp">
