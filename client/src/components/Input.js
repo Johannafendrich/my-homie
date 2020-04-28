5d6f74aedf2e09ded16e4134b5b6c24df0d6535b
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-28 15:47:08 +0200

    Import Container

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
+import InputContainer from '../components/InputContainer';
 
-const Input = styled.input`
+export const Input = styled.input`
   border: none;
   cursor: pointer;
   margin-left: 10px;
@@ -14,17 +15,6 @@ const Input = styled.input`
 &:placeholder {
         color: #9eaebc;
   }
-`;
-
-const InputContainer = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-item: left;
-  border: 1.5px solid #707070;
-  border-radius: 5px;
-  padding: 10px;
-  margin: 10px 0px;
-  outline: none;
 `;
 
 const InputField = ({ type, placeholder, value, src, onChange }) => {
