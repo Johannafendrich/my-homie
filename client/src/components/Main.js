8602ec0113026a8667cd16c654bd43afa0a60aea
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-28 21:00:55 +0200

    delete fix size from Main

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,9 +3,9 @@ import styled from '@emotion/styled';
 const Main = styled.div`
   display: flex;
   width: 100vw;
-  height: 100vh;
+  padding-top: 2rem;
   justify-content: center;
-  flex-flow: column nowrap;
+  flex-flow: column wrap;
   align-items: center;
   margin: 0;
 `;
