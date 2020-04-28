e979bd3177a3afc3f7048358143c870a820766d7
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-28 15:45:22 +0200

    Outsource Container Component

---
+++ client/src/components/InputContainer.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const InputContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-item: left;
+  border: 1.5px solid #707070;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 10px 0px;
+  outline: none;
+`;
+
+export default InputContainer;
