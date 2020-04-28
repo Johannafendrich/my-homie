be9f0fc318ceee3f28bda7ff5d603a8dcf3cafa8
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-28 09:15:55 +0200

    Fix styling issue

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 const Main = styled.div`
   display: flex;
   width: 100vw;
+  height: 100vh;
   justify-content: center;
   flex-flow: column nowrap;
   align-items: center;
