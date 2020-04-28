40761d18797cd0de5c18527847106fac2715b077
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-28 21:49:34 +0200

    Add Text Copmponents and Icon

---
+++ client/src/components/TextWrapper.js
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const TextWrapper = styled.span`
+  display: flex;
+  flex-direction: column
+  justify-content: center;
+  margin-top: 0.5rem;
+  align-items: baseline;
+  align-content: flex-start;
+`;
+
+export default TextWrapper;
--- client/src/components/Title.js
+++ client/src/components/Title.js
@@ -3,6 +3,17 @@ import styled from '@emotion/styled';
 const Title = styled.h1`
   font: 1.5rem 'Anton', sans-serif;
   color: #323338;
+  line-height: 0.5;
+  padding-top: 1.5rem;
+`;
+
+export const TitleWhite = styled.h1`
+  font: 1.7rem 'Anton', sans-serif;
+  color: white;
+  line-height: 1;
+  text-align: left;
+  margin-left: 1.5rem;
+  margin-bottom: 0;
 `;
 
 export default Title;
