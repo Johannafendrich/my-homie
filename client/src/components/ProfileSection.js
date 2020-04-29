58e5b8cb0d3619095ad335a4716f8f6b7d4cee34
Johannafendrich <fendrichjohanna@gmail.com>
2020-04-29 17:45:38 +0200

    Fix Profile Header Area

--- client/src/components/ProfileSection.js
+++ client/src/components/ProfileSection.js
@@ -1,37 +1,39 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import MainBackground from '../assets/background-reverse.svg';
 import Avatar from '../components/Avatar';
 import PlaceholderAvatar from '../assets/profile/avatarPlaceholder.svg';
 import { TitleWhite } from '../components/Title';
 import Textwrapper from '../components/TextWrapper';
 import LocationIcon from '../assets/pin-white.svg';
 import Text from '../components/Text';
+import SectionHeader from './SectionHeader';
 
-const ProfileContainer = styled.div`
+const ProfileWrapper = styled.div`
   display: flex;
+  top: 1rem;
   width: 100vw;
-  margin-top: 2rem;
-  padding: 1rem;
+  padding: 4rem 0 1rem 0;
+  max-width: 500px;
   justify-content: space-evenly;
   flex-direction: row;
   align-items: center;
-  background-image: url(${MainBackground});
-  background-repeat: no-repeat;
-  background-size: cover;
+  align-content: flex-end;
 `;
 
 const ProfileSection = () => (
-  <ProfileContainer>
-    <TitleWhite>
-      Hello, Tina Walters
-      <Textwrapper>
-        <img src={LocationIcon} />
-        <Text>Cologne, Germany</Text>
-      </Textwrapper>
-    </TitleWhite>
-    <Avatar src={PlaceholderAvatar} />
-  </ProfileContainer>
+  <>
+    <ProfileWrapper>
+      <TitleWhite>
+        Hello, Tina Walters
+        <Textwrapper>
+          <img src={LocationIcon} />
+          <Text>Cologne, Germany</Text>
+        </Textwrapper>
+      </TitleWhite>
+      <Avatar src={PlaceholderAvatar} />
+    </ProfileWrapper>
+    <SectionHeader />
+  </>
 );
 
 export default ProfileSection;
