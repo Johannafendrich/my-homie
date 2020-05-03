30b10f96f9b8a82ecd3082ad7ee513a1b9ad7250
Johannafendrich <fendrichjohanna@gmail.com>
2020-05-03 17:20:18 +0200

    Add Test page

--- client/src/pages/CreateAccount.js
+++ client/src/pages/CreateAccount.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import Header from '../components/Header';
 import ProfileForm from '../components/ProfileForm';
-import ProfileSection from '../components/ProfileSection';
+import AddAvatar from '../components/AddAvatar';
 import BottomNav from '../utils/Navigation';
+import styled from '@emotion/styled';
+import { TitleWhite } from '../components/Title';
+import Textwrapper from '../components/TextWrapper';
+import LocationIcon from '../assets/pin-white.svg';
+import Text from '../components/Text';
+import SectionHeader from '../components/SectionHeader';
 
-function AddAccount() {
+const ProfileWrapper = styled.div`
+  display: flex;
+  top: 1rem;
+  width: 100vw;
+  padding: 4rem 0 1rem 0;
+  max-width: 500px;
+  justify-content: space-evenly;
+  flex-direction: row;
+  align-items: center;
+  align-content: flex-end;
+`;
+
+const AddAccount = () => {
   return (
     <>
+      <ProfileWrapper>
+        <TitleWhite>
+          Hello, Tina Walters
+          <Textwrapper>
+            <img src={LocationIcon} />
+            <Text>Cologne, Germany</Text>
+          </Textwrapper>
+        </TitleWhite>
+        <AddAvatar />
+      </ProfileWrapper>
+      <SectionHeader />
       <Header />
-      <ProfileSection />
       <ProfileForm />
       <BottomNav />
     </>
   );
-}
+};
 export default AddAccount;
