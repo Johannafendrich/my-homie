e81aacba637640f12b33d2db7c299165c709897b
Johannafendrich <fendrichjohanna@gmail.com>
2020-05-03 17:26:16 +0200

    Fix avatar components

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -1,57 +1,56 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import cameraIcon from '../assets/camera.svg';
+import PlaceholderAvatar from '../assets/profile/avatarPlaceholder.svg';
 import PropTypes from 'prop-types';
 
 const ProfileImg = styled.img`
-  height: 100px;
+  min-height: 100px;
   width: 100px;
   border-radius: 50%;
-  border: 4px solid #8645ff;
+  background: url(${PlaceholderAvatar}) no-repeat;
   background-size: cover;
+  border: 4px solid #8645ff;
+  object-fit: cover;
   background-color: white;
-  background-position: -15px 0px;
 `;
 
-const UploadImage = styled.button`
+const ImageInput = styled.input`
   display: flex;
-  height: 25px;
-  width: 25px;
-  margin-left: 35px;
-  position: absolute;
-  justify-content: center;
-  border-radius: 50%;
+  height: 20px;
+  width: 20px;
+  opacity: 0%;
+   position relative;
+   border-radius: 50%;
   border: none;
-  background-color: white;
-  outline: none;
-  &:active {
-    transform: translateY(1px) scale(0.9);
-    box-shadow: 0px 5px 10px rgba(0, 0, 0, 20%);
+  focus {
+    outline: none;
   }
+  overflow: hidden;
 `;
 
 const AvatarContainer = styled.div`
   display: flex;
-  width: 140px;
   height: auto;
   flex-direction: row-reverse;
   justify-content: center;
   align-items: flex-end;
 `;
-const Avatar = ({ onClick, src }) => {
+const Avatar = (onChange) => {
   return (
-    <AvatarContainer>
-      <UploadImage onClick={onClick}>
-        <img src={cameraIcon} />
-      </UploadImage>
-      <ProfileImg src={src}></ProfileImg>
-    </AvatarContainer>
+    <>
+      <AvatarContainer>
+        <ImageInput
+          type="file"
+          onChange={onChange}
+          accept=".png, .jpg, .jpeg"
+        />
+        <ProfileImg src="https://res.cloudinary.com/dqbs4ljft/image/upload/v1588503708/uyaqo55cn1brdosoiwgu.jpg" />
+      </AvatarContainer>
+    </>
   );
+};
+Avatar.propTypes = {
+  onChange: PropTypes.func,
 };
 
 export default Avatar;
-
-Avatar.propTypes = {
-  onClick: PropTypes.func,
-  src: PropTypes.any,
-};
