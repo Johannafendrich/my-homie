963d9ae1f10b1dbd5eac260307a848bbe04015b7
Johannafendrich <fendrichjohanna@gmail.com>
2020-05-04 23:03:36 +0200

    Add AvatarProvider to App

--- client/src/App.js
+++ client/src/App.js
@@ -9,37 +9,40 @@ import ProfilePage from './pages/Profile';
 import Main from './components/Main';
 import Welcome from './pages/Welcome';
 import AddAccount from './pages/CreateAccount';
+import AvatarProvider from './utils/context/AvatarProvider';
 
 function App() {
   return (
-    <Router>
-      <GlobalStyles />
-      <Main>
-        <Switch>
-          <Route exact path="/">
-            <Start />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route path="/signUp">
-            <SignUp />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route path="/createAccount">
-            <AddAccount />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/profile">
-            <ProfilePage />
-          </Route>
-        </Switch>
-      </Main>
-    </Router>
+    <AvatarProvider>
+      <Router>
+        <GlobalStyles />
+        <Main>
+          <Switch>
+            <Route exact path="/">
+              <Start />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route path="/signUp">
+              <SignUp />
+            </Route>
+            <Route path="/welcome">
+              <Welcome />
+            </Route>
+            <Route path="/createAccount">
+              <AddAccount />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/profile">
+              <ProfilePage />
+            </Route>
+          </Switch>
+        </Main>
+      </Router>
+    </AvatarProvider>
   );
 }
 
